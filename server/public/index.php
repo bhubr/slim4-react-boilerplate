6e336e4634cc154315209f4485f5515ff1e7126d
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2020-04-21 12:55:54 +0200

    Got a basic working Slim+React setup :rocket:

--- server/public/index.php
+++ server/public/index.php
@@ -6,9 +6,19 @@ use Slim\Factory\AppFactory;
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
-
-$app->get('/', function (Request $request, Response $response, $args) {
+$app->addErrorMiddleware(true, true, true);
+/*$app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
+    return $response;
+});
+*/
+
+$app->get('/[{path:[^.]*}]', function($request, $response, $path = null) {
+    error_log($request->getUri()->getPath());
+    // $index_path = realpath(__DIR__ . '/../build/index.html');
+    $index_path = realpath(__DIR__ . '/_index.html');
+    $index_file = file_get_contents($index_path);
+    $response->getBody()->write($index_file);
     return $response;
 });
 
